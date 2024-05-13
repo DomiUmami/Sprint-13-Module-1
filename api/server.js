e76e50155a6a884c452f152f5e75bbeb59310983
DomiUmami <williams_dominik@yahoo.com>
2024-05-13 09:27:56 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,31 @@
 
 const express = require('express')
 const User = require('./users/model')
+
 const server = express()
+server.use(express.json())
+
+
+server.post('/api/users', (req, res) =>{
+const user = req.body;
+if(!user.name || !user.bio){
+    res.status(400).json({
+        message: "Please provide name and bio for the user",
+    })
+} else {
+User.insert(user)
+    .then(createdUser =>{
+        res.status(201).json(createdUser)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message:'error creating users',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+    }
+})
 
 server.get('/api/users', (req, res) =>{
 User.find()
@@ -18,8 +42,8 @@ User.find()
 })
 
 server.get('/api/users/:id', (req, res) =>{
-    User.findById(req.params.id)
-        .then(user =>{
+User.findById(req.params.id)
+    .then(user =>{
             if(!user){
                 res.status(404).json({
                     message: "The user with the specified ID does not exist",
